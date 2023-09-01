168cffc7ff11a454aeb68a1fcdaebe54ad0ecbc7
Thomas FERREIRA <thomas_ferreira_93330@hotmail.fr>
2023-09-01 12:46:08 +0200

    Add dynamic quiz content

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,5 +6,96 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  quizContent = [
+    {
+      question: 'Quel est le principal avantage des composants dans Angular ?',
+      answers: [
+        'Accélérer le temps de chargement de la page',
+        'Encapsulation et réutilisabilité du code',
+        'Facilité de déploiement',
+        'Optimisation du SEO'
+      ]
+    },
+    {
+      question: 'Quel est le principal avantage des composants dans Angular ?',
+      answers: [
+        'Accélérer le temps de chargement de la page',
+        'Encapsulation et réutilisabilité du code',
+        'Facilité de déploiement',
+        'Optimisation du SEO'
+      ]
+    },
+    {
+      question: 'Quel est le principal avantage des composants dans Angular ?',
+      answers: [
+        'Accélérer le temps de chargement de la page',
+        'Encapsulation et réutilisabilité du code',
+        'Facilité de déploiement',
+        'Optimisation du SEO'
+      ]
+    },
+    {
+      question: 'Quel est le principal avantage des composants dans Angular ?',
+      answers: [
+        'Accélérer le temps de chargement de la page',
+        'Encapsulation et réutilisabilité du code',
+        'Facilité de déploiement',
+        'Optimisation du SEO'
+      ]
+    },
+    {
+      question: 'Quel est le principal avantage des composants dans Angular ?',
+      answers: [
+        'Accélérer le temps de chargement de la page',
+        'Encapsulation et réutilisabilité du code',
+        'Facilité de déploiement',
+        'Optimisation du SEO'
+      ]
+    },
+    {
+      question: 'Quel est le principal avantage des composants dans Angular ?',
+      answers: [
+        'Accélérer le temps de chargement de la page',
+        'Encapsulation et réutilisabilité du code',
+        'Facilité de déploiement',
+        'Optimisation du SEO'
+      ]
+    },
+    {
+      question: 'Quel est le principal avantage des composants dans Angular ?',
+      answers: [
+        'Accélérer le temps de chargement de la page',
+        'Encapsulation et réutilisabilité du code',
+        'Facilité de déploiement',
+        'Optimisation du SEO'
+      ]
+    },
+    {
+      question: 'Quel est le principal avantage des composants dans Angular ?',
+      answers: [
+        'Accélérer le temps de chargement de la page',
+        'Encapsulation et réutilisabilité du code',
+        'Facilité de déploiement',
+        'Optimisation du SEO'
+      ]
+    },
+    {
+      question: 'Quel est le principal avantage des composants dans Angular ?',
+      answers: [
+        'Accélérer le temps de chargement de la page',
+        'Encapsulation et réutilisabilité du code',
+        'Facilité de déploiement',
+        'Optimisation du SEO'
+      ]
+    },
+    {
+      question: 'Quel est le principal avantage des composants dans Angular ?',
+      answers: [
+        'Accélérer le temps de chargement de la page',
+        'Encapsulation et réutilisabilité du code',
+        'Facilité de déploiement',
+        'Optimisation du SEO'
+      ]
+    }
+  ];
 }
